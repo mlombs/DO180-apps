65d40d3e2577f37adcb97826295e3a53928b35ab
Marco Lombardi <marco.lombardi@it-present.com>
2022-05-30 13:32:14 +0000

    daje

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, mi sono rotto i coglioni cazzo" php version is " . PHP_VERSION . "\n";
+print "Hello, mi sono rotto i coglioni cazzoooo" php version is " . PHP_VERSION . "\n";
 ?>
